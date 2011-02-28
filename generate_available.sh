5b96be9697958744d0ee890d6789ad8599f0dff7
Vinay Kumar <sdvinay@gmail.com>
2011-02-28 03:34:29 -0500

    Parse the position and age into separate fields of available.txt

--- generate_available.sh
+++ generate_available.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-echo "id/name/team/amount/time" > available.txt 
+echo "id/name/pos/age/team/amount/time" > available.txt 
 cat ~/trhl/data/stat.txt | awk -F: '($3 < 5 && $3 > 0) \
-	{print $1 "/" $2 "/" $4 "/" $5 "/" 1900+$6 "-"$7 "-" $8"T"$9":"$10":"$11 "-0500"}'| 
+	{	sub(/\-/, "/", $2);
+		sub(/ +/, "/", $2);
+		print $1 "/" $2 "/" $4 "/" $5 "/" 1900+$6 "-"$7 "-" $8"T"$9":"$10":"$11 "-0500"}'| 
 	sort --field-separator=\/ --key 5  >> available.txt  
 cat available.txt
