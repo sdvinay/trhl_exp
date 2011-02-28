3420a9b6bfbcfeb083dc7f587c34dac1d71d852a
Vinay Kumar <sdvinay@gmail.com>
2011-02-28 03:15:05 -0500

    Initial commit of command line to update available.txt from stat.txt

---
+++ generate_available.sh
@@ -0,0 +1,2 @@
+
+cd ~/kumars/vinay/projects/trhl_exp/ ; echo "id/name/team/amount/time" > available.txt ; cat ~/trhl/data/stat.txt | awk -F: '($3 < 5 && $3 > 0) {print $1 "/" $2 "/" $4 "/" $5 "/" 1900+$6 "-"$7 "-" $8"T"$9":"$10":"$11 "-0500"}'| sort --field-separator=\/ --key 5  >> available.txt  ; cat available.txt
