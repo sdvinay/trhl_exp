686b23fc905b6622ecf92e240777b2c96cf72500
Vinay Kumar <sdvinay@gmail.com>
2011-02-28 04:57:29 -0500

    When generating available.txt, include players that haven't been bid on

--- generate_available.sh
+++ generate_available.sh
@@ -2,9 +2,14 @@
 
 
 echo "id/name/pos/age/team/amount/time" > available.txt 
-cat ~/trhl/data/stat.txt | awk -F: '($3 < 5 && $3 > 0) \
+	cat ~/trhl/data/stat.txt | awk -F: '($3 < 5) \
 	{	sub(/\-/, "/", $2);
 		sub(/ +/, "/", $2);
-		print $1 "/" $2 "/" $4 "/" $5 "/" 1900+$6 "-"$7 "-" $8"T"$9":"$10":"$11 "-0500"}'| 
+		if ($3 > 0)
+		{ print $1 "/" $2 "/" $4 "/" $5 "/" 1900+$6 "-"$7 "-" $8"T"$9":"$10":"$11 "-0500"}
+		else
+		{ print $1 "/" $2 "///"}
+	}'|
 	sort --field-separator=\/ --key 5  >> available.txt  
+
 cat available.txt
